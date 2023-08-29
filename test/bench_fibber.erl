3ac06debc8b1a8387a3c840bfc6941f78359fc59
Fred Youhanaie <fyrlang@anydata.co.uk>
2023-08-29 23:51:47 +0100

    add more, shorter benchmarks

--- test/bench_fibber.erl
+++ test/bench_fibber.erl
@@ -1,18 +1,42 @@
+%%%-------------------------------------------------------------------
 %%% @author Fred Youhanaie <fyrlang@anydata.co.uk>
 %%% @copyright 2023, Fred Youhanaie
 %%% @doc
 %%%
-%%% Set of benchmarks,
+%%% Set of benchmarks for working with rebar3_bench.
 %%%
 %%% @end
 %%% Created : 29 Aug 2023 by Fred Youhanaie <fyrlang@anydata.co.uk>
+%%%-------------------------------------------------------------------
 
 -module(bench_fibber).
 
--export([ fib_1/1, bench_fib_1/2 ]).
+-export([ fib_2/1, bench_fib_2/2 ]).
+-export([ fib_5/1, bench_fib_5/2 ]).
+-export([ fib_10/1, bench_fib_10/2 ]).
 
-fib_1(_) ->
+%%--------------------------------------------------------------------
+
+fib_2(_) ->
     ok.
 
-bench_fib_1(_, _) ->
-    fibber:fib(20).
+bench_fib_2(_, _) ->
+    fibber:fib(2).
+
+%%--------------------------------------------------------------------
+
+fib_5(_) ->
+    ok.
+
+bench_fib_5(_, _) ->
+    fibber:fib(5).
+
+%%--------------------------------------------------------------------
+
+fib_10(_) ->
+    ok.
+
+bench_fib_10(_, _) ->
+    fibber:fib(10).
+
+%%--------------------------------------------------------------------
